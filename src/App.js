a63b7257b71a4da43c347b50bef504913be2c709
Keith <keithvip@gmail.com>
2022-09-15 20:14:00 -0500

    trying to create transions from landing page

--- src/App.js
+++ src/App.js
@@ -8,42 +8,59 @@ import Portfolio from './components/Portfolio';
 // import logo from './assets/images/banner.gif';
 import './App.css';
 import './output.css';
+import { useTimeoutFn } from 'react-use';
+import { Transition } from '@headlessui/react';
+
 
 function App() {
   const [aboutSelected, setAboutSelected] = useState(true);
   const [portfolioSelected, setPortfolioSelected] = useState(false);
   const [resumeSelected, setResumeSelected] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  // const [resetIsShowing] = useTimeoutFn(() => setisLoading(true), 500)
+
 
   useEffect(() => {
     setTimeout(() => {
-      setLoading(false);
+      setIsLoading(false);
     }, 5000);
   }, []);
 
   return (
     <div>
-      {loading ? (
-        <Intro></Intro>
-      ) : (
-        <main>
-          <Navigation
-            aboutSelected={aboutSelected}
-            setAboutSelected={setAboutSelected}
-            portfolioSelected={portfolioSelected}
-            setPortfolioSelected={setPortfolioSelected}
-            resumeSelected={resumeSelected}
-            setResumeSelected={setResumeSelected}
-          ></Navigation>
-          {aboutSelected ? (
-            <About></About>
-          ) : portfolioSelected ? (
-            <Portfolio></Portfolio>
-          ) : (
-            <Resume></Resume>
-          )}
-        </main>
-      )}
+      {isLoading ?
+        (
+          // <Transition
+          //   // appear={true}
+          //   show={isLoading}
+          //   enter="transition-opacity ease-in-out duration-50"
+          //   enterFrom="opacity-0"
+          //   enterTo="opacity-100"
+          //   leave="transition-opacity ease-in-out duration-150"
+          //   leaveFrom="opacity-100"
+          //   leaveTo="opacity-0"
+          // >
+          <Intro></Intro>
+          // </Transition>
+        ) : (
+          <main>
+            <Navigation
+              aboutSelected={aboutSelected}
+              setAboutSelected={setAboutSelected}
+              portfolioSelected={portfolioSelected}
+              setPortfolioSelected={setPortfolioSelected}
+              resumeSelected={resumeSelected}
+              setResumeSelected={setResumeSelected}
+            ></Navigation>
+            {aboutSelected ? (
+              <About></About>
+            ) : portfolioSelected ? (
+              <Portfolio></Portfolio>
+            ) : (
+              <Resume></Resume>
+            )}
+          </main>
+        )}
     </div>
   );
 }
