ea65bcebd4e151b2400af76a6b7c6a705b884789
Keith <keithvip@gmail.com>
2022-10-10 19:03:28 -0500

    resume resizing

--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -15,6 +15,15 @@ const Featured = () => {
     const projectData = [
         {
             id: '1',
+            name: 'Woofify',
+            description: 'Use this simple search that populates information on specific dog breeds and see if they will be a good fit for your family. After finding a breed that interests you, you can locate them in your local area! A one stop shop for finding your new dog to adopt. Thinking about getting a dog? Would rather adopt one than go to a dogon breeder? Well, now you can with Woofify!',
+            website: 'https://keithvip.github.io/Woofify/',
+            alt: 'woofify',
+            technologies: 'JavaScript, HTML, CSS, JQuery, Restful Api, Bulma',
+            image: WoofifyImg
+        },
+        {
+            id: '2',
             name: 'Slayers',
             description: 'A group of nerds came together with one simple idea, create an online RPG portal that takes tabletop gaming to the next level. This web application allows a user to create a character, build basic statistics, and create or join a party through the use of simple input.',
             website: 'https://fierce-temple-92608.herokuapp.com/',
@@ -23,22 +32,13 @@ const Featured = () => {
             image: SlayersImg
         },
         {
-            id: '2',
+            id: '3',
             name: 'Book Search Engine',
             description: 'Take a fully functioning Google Books API search engine built with a Restful API, and refactor it to be a GraphQL API built with Apollo Server.',
             website: 'https://calm-badlands-97625.herokuapp.com/',
             alt: 'googlebook',
             technologies: 'Mern, React, MongoDB, Node.js',
             image: GoogleBookImg
-        },
-        {
-            id: '3',
-            name: 'Woofify',
-            description: 'Use this simple search that populates information on specific dog breeds and see if they will be a good fit for your family. After finding a breed that interests you, you can locate them in your local area! A one stop shop for finding your new dog to adopt. Thinking about getting a dog? Would rather adopt one than go to a dogon breeder? Well, now you can with Woofify!',
-            website: 'https://keithvip.github.io/Woofify/',
-            alt: 'woofify',
-            technologies: 'JavaScript, HTML, CSS, JQuery, Restful Api, Bulma',
-            image: WoofifyImg
         },
     ];
 
@@ -47,7 +47,7 @@ const Featured = () => {
             <ul id='container' className='customViewHeight flex flex-col items-center'>
                 {projectData.map(({ id, name, description, website, alt, technologies, image }) => (
                     <li key={id} className='max-w-xs flex flex-col justify-center items-center'>
-                        <h2 className='font-Ephesis font-bold flex justify-end text-6xl'>{name}</h2>
+                        <h2 className='font-Ephesis font-bold flex justify-end text-center text-6xl'>{name}</h2>
                         <a href={website} alt={alt} target='_blank'>
                             <img
                                 data-aos='zoom-in'
@@ -60,7 +60,7 @@ const Featured = () => {
                                 data-aos-once='false'
                                 src={image}
                                 alt={alt}
-                                className='flex m-2 p-1 ring-c-black ring-4 rounded-3xl'
+                                className='flex my-2 p-1 ring-c-black ring-4 rounded-3xl'
                             ></img>
                         </a>
                         <div
@@ -72,7 +72,7 @@ const Featured = () => {
                             data-aos-anchor=''
                             data-aos-achor-placement='top-bottom'
                             data-aos-once='false'
-                            className='relative w-4/5 laptop:right-32 laptop:top-28'>
+                            className='relative justify-center w-4/5 laptop:right-32 laptop:top-28'>
                             <p className='text-sm flex flex-wrap text-justify bg-c-yellow rounded-lg p-3 laptop:max-w-xl max-h-fit'>
                                 {description}
                             </p>
