1b89af03dfb06ab44fe335315bd99932ca64bf99
Keith <keithvip@gmail.com>
2022-10-20 16:47:37 -0500

    changers to server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 // const db= require('./config/connection');
-// const path = require('path');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
