c1d7aad9e13a892ac20657fb8eff588edf5175c3
Keith <keithvip@gmail.com>
2022-10-25 15:20:04 -0500

    merge conflict

--- client/src/components/Featured/index.js
+++ client/src/components/Featured/index.js
@@ -43,11 +43,11 @@ const Featured = () => {
     ];
 
     return (
-        <section id='featured' className=''>
+        <section id='featured' className='bg-paper'>
             <ul id='container' className='customViewHeight flex flex-col items-center'>
                 {projectData.map(({ id, name, description, website, alt, technologies, image }) => (
-                    <li key={id} className='max-w-xs flex flex-col justify-center items-center'>
-                        <h2 className='font-cutive font-bold flex justify-end text-center text-7xl'>{name}</h2>
+                    <li key={id} className='flex flex-col justify-center items-center max-w-xs tablet:max-w-xl laptop:max-w-2xl desktop:max-w-4xl'>
+                        <h2 className='font-Cutive font-black flex justify-end text-center text-7xl laptop:text-6xl desktop:text-7xl tablet:text-5-xl'>{name}</h2>
                         <a href={website} alt={alt} target='_blank'>
                             <img
                                 data-aos='zoom-in'
@@ -72,8 +72,8 @@ const Featured = () => {
                             data-aos-anchor=''
                             data-aos-achor-placement='top-bottom'
                             data-aos-once='false'
-                            className='relative justify-center w-4/5 laptop:right-32 laptop:top-28'>
-                            <p className='text-sm flex flex-wrap text-center bg-c-yellow rounded-lg p-3 laptop:max-w-xl max-h-fit'>
+                            className='justify-center w-4/5 laptop:right-32 laptop:top-28'>
+                            <p className='font-Cutive font-extrabold text-sm flex flex-wrap text-center bg-c-yellow rounded-lg p-3 laptop:max-w-xl max-h-fit'>
                                 {description}
                             </p>
                             <div className='font-bold italic flex justify-center text-sm text-center'>
