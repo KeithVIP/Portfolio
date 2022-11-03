08b3a9d248290456cff5db08787c561b8853935c
Keith <keithvip@gmail.com>
2022-11-03 18:19:43 -0500

    contact section updated and fully functional

--- client/src/pages/ContactPage.js
+++ client/src/pages/ContactPage.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Contact from '../components/Contact';
-import Footer from '../components/Footer';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const ContactPage = () => {
     return (
         <>
-            <Header />
-            <Contact />
-            <Footer />
-            <div className='flex justify-center'>&copy; {new Date().getFullYear()}All rights reserved.</div>
+            <div className='bg-paper'>
+                <Header />
+                {/* <div className='relative h-screen flex justify-center items-center space-x-40'>
+                <h2 className='font-League font-bold bg-c-blue text-5xl h-1/5 w-full flex justify-start items-center'>Hey, let's talk...</h2>
+            </div> */}
+                <Contact />
+                <Footer />
+                <div className='flex justify-center'>&copy; {new Date().getFullYear()} All rights reserved.</div>
+            </div>
         </>
     );
 };
